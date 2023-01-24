5d426782b8be6bdac4db36e259278afaa75b9da0
dennyArfansyah <dennyarfansyah@gmail.com>
2023-01-25 00:54:49 +0700

    Initial Commit

---
+++ Bookworm/BookwormApp.swift
@@ -0,0 +1,17 @@
+//
+//  BookwormApp.swift
+//  Bookworm
+//
+//  Created by Denny Arfansyah on 25/01/23.
+//
+
+import SwiftUI
+
+@main
+struct BookwormApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
